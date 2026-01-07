ddad1536e454f0aa0e42656cdb23aa9eb906ebe5
Kyle Throssell <kthrossell@tetrascience.com>
2026-01-07 10:06:51 -0500

    added new feature

--- again_and_again/__init__.py
+++ again_and_again/__init__.py
@@ -1,5 +1,6 @@
 """again-and-again: Personal utility library for things I do again and again."""
 
+from again_and_again.src.git_wizard import get_git_repo_root_path
 from again_and_again.src.path_wizard import normalize_file_path
 
-__all__ = ["normalize_file_path"]
+__all__ = ["normalize_file_path", "get_git_repo_root_path"]
---
+++ again_and_again/src/git_wizard.py
@@ -0,0 +1,12 @@
+import pathlib
+
+
+def get_git_repo_root_path() -> pathlib.Path:
+    """
+    Get the root path of the current git repository.
+    """
+    current_path = pathlib.Path.cwd()
+    for parent in [current_path, *current_path.parents]:
+        if (parent / ".git").exists():
+            return parent
+    raise FileNotFoundError(f"Could not identify a git repository starting from {current_path}")
