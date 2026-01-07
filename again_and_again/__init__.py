96f891769fe8262a4dd719dfdc65620f58295fb9
Kyle Throssell <kthrossell@tetrascience.com>
2026-01-07 09:52:58 -0500

    updated version and function signature

--- again_and_again/__init__.py
+++ again_and_again/__init__.py
@@ -1,5 +1,5 @@
 """again-and-again: Personal utility library for things I do again and again."""
 
-from again_and_again.src.path_wizard import normalize_path
+from again_and_again.src.path_wizard import normalize_file_path
 
-__all__ = ["normalize_path"]
+__all__ = ["normalize_file_path"]
