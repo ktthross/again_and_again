5c7f620df4199a03a96be6e57e6ffab41e6a2d6f
Kyle Throssell <kthrossell@tetrascience.com>
2026-01-08 15:33:12 -0500

    responding to pre-commit

--- again_and_again/__init__.py
+++ again_and_again/__init__.py
@@ -1,6 +1,7 @@
 """again-and-again: Personal utility library for things I do again and again."""
 
 from again_and_again.src.git_wizard import get_git_repo_root_path
+from again_and_again.src.gpu_wizard import get_device
 from again_and_again.src.path_wizard import normalize_file_path
 
-__all__ = ["normalize_file_path", "get_git_repo_root_path"]
+__all__ = ["normalize_file_path", "get_git_repo_root_path", "get_device"]
